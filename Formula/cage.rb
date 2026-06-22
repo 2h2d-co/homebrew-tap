b3b1c21a21fbbed5db009e122420050557af217f
Kaan Ozdokmeci <kaan@ozdokmeci.com>
2026-06-22 03:04:39 +0200

    Update cage to v0.0.9

--- Formula/cage.rb
+++ Formula/cage.rb
@@ -1,10 +1,10 @@
 class Cage < Formula
   desc "Load 1Password Environments with age-protected service account tokens"
   homepage "https://github.com/2h2d-co/cage"
-  url "https://github.com/2h2d-co/cage/releases/download/v0.0.8/cage_0.0.8_darwin_arm64.tar.gz"
-  sha256 "dd8a2f6ee65d943f1ce75dd12bfe8283fe43351c3d988b6c7432a5159b0cda31"
+  url "https://github.com/2h2d-co/cage/releases/download/v0.0.9/cage_0.0.9_darwin_arm64.tar.gz"
+  sha256 "be924e534f77fea8b5fceccd0f518e329fb1cd86dc1dd1d719e80a020036a0df"
   license "MIT"
-  version "0.0.8"
+  version "0.0.9"
 
   depends_on :macos
   depends_on arch: :arm64
